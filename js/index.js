3b513027097e86766740237885605d2fbbd1b9b2
danielmedtar <dmedinat.ar@gmail.com>
2021-08-04 18:51:49 -0300

    update

--- js/index.js
+++ js/index.js
@@ -6,22 +6,19 @@ function muestraProyectos(proyectos) {
         
         for(const proyecto of proyectos) {
             contenedorProyectos.append(`
-
-            <li>
-                <img class="img-portfolio" src="${proyecto.img}" alt="${proyecto.alt}">
-                <div class="caption">
+            <div class="card-proyectos">
                 <h3 class="proyecto-nombre">${proyecto.nombre}</h3>
                 <h5 class="proyecto-techs">${proyecto.Techs}</h5>
-                    <div class="container-botones">    
-                        <button class="boton-link">
-                            <a href="${proyecto.linkSitio}" target="_blank">Ir al sitio</a>
-                        </button>
-                        <button class="boton-link">
-                            <a href="${proyecto.linkGithub}" target="_blank">Ir al código</a>
-                        </button>   
-                    </div>
-                </div>
-            </li>
+                <a href="${proyecto.linkSitio}" target="_blank">
+                    <img class="img-portfolio" src="${proyecto.img}" alt="${proyecto.alt}">
+                </a>
+                    
+                <button class="boton-link">
+                    <a href="${proyecto.linkGithub}" target="_blank">Ir al código</a>
+                </button>   
+                    
+            </div>
+            
             `                    
         )}
 }
@@ -34,13 +31,13 @@ function muestraTestimonios(testimonios) {
         
     for(const testimonio of testimonios) {
         contenedorTestimonios.append(`
-         <blockquote>
+        <blockquote>
             <a href="${testimonio.link}" target="_blank">
                 <img class="img-testimonio" src="${testimonio.img}" alt="${testimonio.alt}">
             </a>
-             <h3>${testimonio.nombre}</h3>             
-             <q>${testimonio.palabras}</q>
-         </blockquote>
-         `                    
+            <h3>${testimonio.nombre}</h3>             
+            <q>${testimonio.palabras}</q>
+        </blockquote>
+        `                    
     )}
 }
