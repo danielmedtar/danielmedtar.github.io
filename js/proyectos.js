30e3c4540d1ccbd6093400cf644ff4090045761c
danielmedtar <dmedinat.ar@gmail.com>
2021-08-16 21:41:16 -0300

    agregando lista de tareas como proyecto

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,5 +1,6 @@
 let proyectos = [
     {id: 1, nombre: "Old Portfolio", Techs: "HTML5 y CSS3", img: 'img/oldportfolio.png', linkSitio: 'https://danielmedtar.github.io/old_portfolio/', linkGithub: 'https://github.com/danielmedtar/old_portfolio', alt: 'imagen portfolio'},
     {id: 2, nombre: "Aneplanner - Community Manager", Techs: "HTML5, CSS3, Bootstrap, SASS, GIT", img: 'img/aneplanner.png', linkSitio: 'https://danielmedtar.github.io/Aneplanner/', linkGithub: 'https://github.com/danielmedtar/Aneplanner', alt: 'imagen aneplanner'},
-    {id: 3, nombre: "Developer Shop", Techs: "HTML5, CSS3, Bootstrap, JavaScript, jQuery", img: 'img/developer_shop.png', linkSitio: 'https://danielmedtar.github.io/final_coderjs/', linkGithub: 'https://github.com/danielmedtar/final_coderjs', alt: 'imagen devshop'}
+    {id: 3, nombre: "Developer Shop", Techs: "HTML5, CSS3, Bootstrap, JavaScript, jQuery", img: 'img/developer_shop.png', linkSitio: 'https://danielmedtar.github.io/final_coderjs/', linkGithub: 'https://github.com/danielmedtar/final_coderjs', alt: 'imagen devshop'},
+    {id: 4, nombre: "Lista de Tareas", Techs: "HTML5, CSS3, JavaScript", img: 'img/todolist.png', linkSitio: 'https://danielmedtar.github.io/todolist/', linkGithub: 'https://github.com/danielmedtar/todolist', alt: 'imagen lista de tareas'}
 ]
