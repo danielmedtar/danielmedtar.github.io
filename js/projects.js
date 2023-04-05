4745add139f9ff16aba8f00f8aac5ea85d7cdfab
danielmedtar <dmedinat.ar@gmail.com>
2023-04-05 09:45:26 -0300

    modifying english javascript file

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,9 @@
 let proyectos = [
-    {nombre: "Aneplanner - Community Manager", Techs: "HTML5, CSS3, Bootstrap, SASS, GIT", img: 'img/aneplanner.png', linkSitio: 'https://danielmedtar.github.io/Aneplanner/', linkGithub: 'https://github.com/danielmedtar/Aneplanner', alt: 'imagen aneplanner'},
+    {nombre: "Argentinian salary calculator", Techs: "HTML5, CSS3, JavaScript", img: 'img/calculadora-sueldos.png', linkSitio: 'https://danielmedtar.github.io/calculadora-sueldo/', linkGithub: 'https://github.com/danielmedtar/calculadora-sueldo', alt: 'imagen calculadora de sueldos'},
     {nombre: "Developer Shop", Techs: "HTML5, CSS3, Bootstrap, React JS", img: 'img/developer_shop.png', linkSitio: 'https://danielmedina-proyectoreact.netlify.app/', linkGithub: 'https://github.com/danielmedtar/proyecto-reactjs', alt: 'imagen devshop'},
-    {nombre: "App Clima", Techs: "HTML5, CSS3, JavaScript, API Open Weather", img: 'img/app_clima.png', linkSitio: 'https://danielmedina-appclima.netlify.app', linkGithub: 'https://github.com/danielmedtar/app_clima', alt: 'imagen app clima'},
-    {nombre: "Lista de Tareas", Techs: "HTML5, CSS3, JavaScript", img: 'img/todolist.png', linkSitio: 'https://danielmedtar.github.io/todolist/', linkGithub: 'https://github.com/danielmedtar/todolist', alt: 'imagen lista de tareas'},
-    {nombre: "Calculadora", Techs: "HTML5, CSS3, JavaScript", img: 'img/calculadora.png', linkSitio: 'https://danielmedtar.github.io/Calculadora-JS/', linkGithub: 'https://github.com/danielmedtar/Calculadora-JS', alt: 'imagen calculadora'}
+    {nombre: "Weather App", Techs: "HTML5, CSS3, JavaScript, API Open Weather", img: 'img/app_clima.png', linkSitio: 'https://danielmedina-appclima.netlify.app', linkGithub: 'https://github.com/danielmedtar/app_clima', alt: 'imagen app clima'},
+    {nombre: "Calculator", Techs: "HTML5, CSS3, JavaScript", img: 'img/calculadora.png', linkSitio: 'https://danielmedtar.github.io/Calculadora-JS/', linkGithub: 'https://github.com/danielmedtar/Calculadora-JS', alt: 'imagen calculadora'},
+    {nombre: "Aneplanner - Community Manager", Techs: "HTML5, CSS3, Bootstrap, SASS, GIT", img: 'img/aneplanner.png', linkSitio: 'https://danielmedtar.github.io/Aneplanner/', linkGithub: 'https://github.com/danielmedtar/Aneplanner', alt: 'imagen aneplanner'}
 ]
 
 const contenedorProyectos = $('#container-proyectos')
