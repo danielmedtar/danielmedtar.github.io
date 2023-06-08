1362f83c59a116043f75b5cb770848689edc3a2f
danielmedtar <dmedinat.ar@gmail.com>
2023-06-08 09:33:02 -0300

    modifying php file

--- form.php
+++ form.php
@@ -1,6 +1,26 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$nombre = $_POST['name'];
+$mail = $_POST['email'];
+$telefono = $_POST['phone'];
+$mensaje = $_POST['message'];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Su correo electrónico es " . $mail . ",\r\n";
+$mensaje = "Y su teléfono es " . $telefono . ",\r\n";
+$mensaje = "Y dice: " . $_POST['mensaje'] . ",\r\n";
+$mensaje = "Enviado el " . date('d/m/Y', time());
+
+$destinatario = 'dmedinat.ar@gmail.com';
+$asunto = 'Este mail fue enviado desde tu portfolio';
+
+mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:gracias.html')
+
+?>
+
+<!-- if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
     $mail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $telefono = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
@@ -31,24 +51,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         echo 'Hubo un error al intentar enviar el mensaje. Por favor intenta de nuevo más tarde.';
     }
-}
-
-// $nombre = $_POST['name'];
-// $mail = $_POST['email'];
-// $telefono = $_POST['phone'];
-// $mensaje = $_POST['message'];
-
-// $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-// $mensaje = "Su correo electrónico es " . $mail . ",\r\n";
-// $mensaje = "Y su teléfono es " . $telefono . ",\r\n";
-// $mensaje = "Y dice: " . $_POST['mensaje'] . ",\r\n";
-// $mensaje = "Enviado el " . date('d/m/Y', time());
-
-// $destinatario = 'dmedinat.ar@gmail.com';
-// $asunto = 'Este mail fue enviado desde tu portfolio';
-
-// mail($destinatario, $asunto, utf8_decode($mensaje), $header);
-
-// header('Location:gracias.html')
-
-?>
+} -->
